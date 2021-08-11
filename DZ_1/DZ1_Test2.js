dfb310a51acdea4c22963ab98bb436b9bfb9bab8
ssid11 <77233033+ssid11@users.noreply.github.com>
2021-08-11 14:56:49 +0300

    Create DZ1_Test1.js

---
+++ DZ_1/DZ1_Test2.js
@@ -0,0 +1,9 @@
+let name;
+var admin;
+name = 'Василий';
+admin = name;
+console.log(`Переменная admin содержит значение ${admin}`)
+/*
+Вывод скрипта:
+Переменная admin содержит значение Василий
+*/
