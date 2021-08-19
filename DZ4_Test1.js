1b4724d847c3bb3908a29a7d33931f647504c1ee
ssid11 <77233033+ssid11@users.noreply.github.com>
2021-08-19 11:42:57 +0300

    update Test1

--- DZ4_Test1.js
+++ DZ4_Test1.js
@@ -1,6 +1,6 @@
 function createObject(num){
     if (num < 0 || num > 999){
-        console.log('Wrong number pass!')
+        console.log('Wrong number pass!');
         return {}
     }
 var MyObj = {
@@ -9,4 +9,16 @@ var MyObj = {
     ones: +(""+num).slice(2,3)
 }
 return MyObj
+}
+
+function createObject_1(num){
+    var MyObj = {}
+    if (num < 0 || num > 999){
+        console.log('Wrong number pass!');
+        return MyObj
+    }
+    MyObj.hundreds = Math.floor(num / 100);
+    MyObj.tens = Math.floor((num - MyObj.hundreds * 100) / 10);
+    MyObj.ones = num - MyObj.hundreds * 100 - MyObj.tens * 10;
+    return MyObj
 }
