71b91c1b27798de32ef5c04e50add36a953ee27c
ssid <evp.alcor@yandex.ru>
2021-08-28 23:52:48 +0300

    Test_3 rewrite

--- DZ3/DZ3_Test3.js
+++ DZ3/DZ3_Test3.js
@@ -1,8 +1,23 @@
-let basket = [1, 3, 5, 7];
+let basket = [
+    {
+        name: 'peach',
+        price: 10,
+        qty: 1
+    },
+    {
+        name: 'apple',
+        price: 20,
+        qty: 2
+    },
+    {
+        name: 'banana',
+        price: 30,
+        qty: 3
+    }];
 
-function countBasketPrice(basket){
-    let sum=0;
-    for (let idx=0;idx < basket.length;sum+=basket[idx], idx++){};
+function countBasketPrice(basket) {
+    let sum = 0;
+    for (let idx = 0; idx < basket.length; sum += basket[idx].qty * basket[idx].price, idx++) { };
     return sum;
 };
-console.log(countBasketPrice(basket));2
+console.log(countBasketPrice(basket)); 2
